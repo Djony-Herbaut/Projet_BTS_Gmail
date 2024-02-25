e3dcc9cba7d3cae5edb7c63a5241cb4ef2b750d0
root <Djony-Herbaut@github.com>
2024-02-25 04:02:37 +0100

    [Form] Mise en place connection et envoie a la bdd]

--- controller/main.inc.php
+++ controller/main.inc.php
@@ -1,7 +1,3 @@
-<?php
-include_once __DIR__."/controller/controller.inc.php";
-?>
-
 <main >
 
 <div id="scroll" class="button-scroll">
@@ -20,10 +16,87 @@ include_once __DIR__."/controller/controller.inc.php";
         Immédiatement les nouveaux messages et choisissez <br>
         ceux vous souhaitez lire en priorité.
     </p>
+    <?php
+
+    include_once __DIR__."/connexion.php";
+    $serveur = "localhost"; $dbname = "FakeGmail"; $dbtable = "User"; $user = "root"; $pass = "";
+
+    $prenom = valid_donnees($_POST["prenom"]);
+    $nom = valid_donnees($_POST["nom"]);
+    $mail = valid_donnees($_POST["mail"]);
+    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+
+
+    function valid_donnees($donnees){
+        $donnees = trim($donnees);
+        $donnees = stripslashes($donnees);
+        $donnees = htmlspecialchars($donnees);
+        return $donnees;
+    }
+    session_start();
+
+    echo "<li><h2>Voici vos informations :</h2></li>
+    <li><strong>Nom :</strong> $nom</li>
+    <li><strong>Prénom :</strong> $prenom</li>
+    <li><strong>Âge :</strong> $mail</li>";
+
+    if (!empty($prenom)
+        && strlen($prenom) <= 20
+        && strlen($nom) <= 20
+        && preg_match("/^[A-Za-z '-]+$/",$prenom)
+        && preg_match("/^[A-Za-z '-]+$/",$nom)
+        && !empty($mail)
+        && filter_var($mail, FILTER_VALIDATE_EMAIL)){
+
+
+        try{
+            //On se connecte à la BDD
+            $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
+            $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            if($dbco){
+                // on créer la requête
+                $requete = "CREATE TABLE IF NOT EXISTS $dbname.$dbtable (
+                            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+                            nom VARCHAR( 20 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+                            prenom VARCHAR( 20 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+                            email VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+                            password VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+                            ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;";
+            
+                // on prépare et on exécute la requête
+                $dbco->prepare($requete)->execute();
+            }
+
+            //On insère les données reçues
+
+            $sth = $dbco->prepare("
+                INSERT INTO User(nom, prenom, mail, password)
+                VALUES(:nom, :prenom, :mail, :password)");
+            $sth->bindParam(':nom',$nom);
+            $sth->bindParam(':prenom',$prenom);
+            $sth->bindParam(':mail',$mail);
+            $sth->bindParam(':password',$password);
+            $sth->execute();
+
+            if (header("Location: connexion.php")) {
+                die("Redirection vers connexion.php effectuée avec succès.");
+            } else {
+                die("Erreur lors de la redirection vers connexion.php.");
+            }
+
+        }
+        catch(PDOException $e){
+            echo 'Erreur : '.$e->getMessage();
+        }
+    }else{
+        header("Location:index.php");
+    }
+?>
     <div class="inner-form" role="form">
         <fieldset>
             <legend><h2>Créer un compte</h2></legend>
-            <form action="connexion.php" method="post"> <!-- mettre le nom du fichier html dans aciton -->
+            <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post"> 
                 <label for="nom">Nom *</label>
                 <input type="text" id="nom" name="nom" placeholder="Nom" aria-required="true"  autofocus>
 
@@ -45,6 +118,4 @@ include_once __DIR__."/controller/controller.inc.php";
     </div>
 </section>
 </main>
-<?php
-    include_once __DIR__."/connexion.php";
-?>
+
